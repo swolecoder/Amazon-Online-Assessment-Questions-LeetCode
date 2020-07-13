076505943a739cea30dd4157cd3beea13a9ef3bb
swolecoder <swolecoder@gmail.com>
2020-07-13 06:09:39 -0400

    adding dice

---
+++  Number of Dice Rolls With Target Sum/index.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} d
+ * @param {number} f
+ * @param {number} target
+ * @return {number}
+ */
+
+
+
+var numRollsToTarget = function (d, f, target) {
+    const mod = 10000000007;
+    let map = new Map();
+    if (target < d || target > d * f) return 0;
+
+    if (d == 1) return target <= f ? 1 : 0;
+
+    let k = `${d}${f}${target}`;
+
+    if (!map.has(k)) {
+
+
+        let ways = 0;
+        for (let i = 1; i <= f; i++) {
+            ways += numRollsToTarget(d - 1, f, target - i);
+            ways %= mod
+        }
+
+        map.set(k, ways)
+
+    }
+    return map.get(k)
+};
+
+let d = 1, f = 6, target = 3;
+console.log(numRollsToTarget(d, f, target));
+d = 2, f = 6, target = 7;
+console.log(numRollsToTarget(d, f, target));
+d = 2, f = 5, target = 10;
+console.log(numRollsToTarget(d, f, target));
+d = 30, f = 30, target = 500;
+console.log(numRollsToTarget(d, f, target));
